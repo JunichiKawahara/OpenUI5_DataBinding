1e3a1b6e162f99a7c2a11b0408806461549c32ff
Junichi Kawahara <kawahara-j@hi-ad.jp>
2019-07-29 17:19:09 +0900

    Step 12: Aggregation Binding Using Templates

--- webapp/index.js
+++ webapp/index.js
@@ -7,6 +7,10 @@ sap.ui.require([
 
     // Attach an anonymous function to the SAPUI5 'init' event
     sap.ui.getCore().attachInit(function () {
+        var oProductModel = new JSONModel();
+        oProductModel.loadData("./model/Products.json");
+        sap.ui.getCore().setModel(oProductModel, "products");
+
         var oModel = new JSONModel({
             firstName: "Harry",
             lastName: "Hawk",
@@ -17,18 +21,24 @@ sap.ui.require([
                 zip: "69190",
                 country: "Germany"
             },
+            "salesToDate": 12345.6789,
+            "currencyCode": "EUR"
         });
 
         sap.ui.getCore().setModel(oModel);
 
-	var oResourceModel = new ResourceModel({
+        var oResourceModel = new ResourceModel({
             bundleName: "sap.ui.demo.db.i18n.i18n"
-	});
-	
-	sap.ui.getCore().setModel(oResourceModel, "i18n");
+        });
 
-        new XMLView({
+        sap.ui.getCore().setModel(oResourceModel, "i18n");
+
+        var oView = new XMLView({
             viewName: "sap.ui.demo.db.view.App"
         }).placeAt("content");
+
+        sap.ui.getCore().getMessageManager().registerObject(oView, true);
+
+        oView.placeAt("content");
     });
 });
